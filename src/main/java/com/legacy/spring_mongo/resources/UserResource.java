e64eff17148f663410cea475f12b90fde4db68ae
Tiago Mendes <tiago.mendes.goes@gmail.com>
2024-03-02 19:45:47 -0300

    Get posts for user id

--- src/main/java/com/legacy/spring_mongo/resources/UserResource.java
+++ src/main/java/com/legacy/spring_mongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.legacy.spring_mongo.domain.Post;
 import com.legacy.spring_mongo.domain.User;
 import com.legacy.spring_mongo.dto.UserDTO;
 import com.legacy.spring_mongo.services.UserService;
@@ -62,4 +63,11 @@ public class UserResource {
 		service.update(obj);
 		return ResponseEntity.noContent().build();	
 	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
 }
