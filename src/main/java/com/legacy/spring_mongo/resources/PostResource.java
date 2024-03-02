f79fe682261b03ba402d7dcb71a3961e3a17c81f
Tiago Mendes <tiago.mendes.goes@gmail.com>
2024-03-02 19:57:00 -0300

    PostResource and PostService create

---
+++ src/main/java/com/legacy/spring_mongo/resources/PostResource.java
@@ -0,0 +1,52 @@
+package com.legacy.spring_mongo.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.legacy.spring_mongo.domain.Post;
+import com.legacy.spring_mongo.services.PostService;
+
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+	@GetMapping()
+	public ResponseEntity<List<Post>> findAll() {
+		List<Post> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Void> save(@RequestBody Post obj){
+		service.save(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value="/{id}")
+ 	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ src/main/java/com/legacy/spring_mongo/services/PostService.java
@@ -0,0 +1,36 @@
+package com.legacy.spring_mongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.legacy.spring_mongo.domain.Post;
+import com.legacy.spring_mongo.repositories.PostRepository;
+import com.legacy.spring_mongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public List<Post> findAll() {
+		return repository.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found: " + id));
+	}
+
+	public Post save(Post obj) {
+		return repository.save(obj);
+	}
+
+	public void deleteById(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+}
